259444ab94c7d0d0d482def0324553d5cf134324
Ritesh Singh <rootritesh64@gmail.com>
2020-12-10 11:04:49 +0530

    nav1 50% completed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
 import Header from './Header/Header';
-
+import Nav1 from './Nav1/Nav1';
 function App() {
   return (
     <div className="App">
       <Header></Header>
+      <Nav1></Nav1>
     </div>
   );
 }
---
+++ src/Nav1/Nav1.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './Nav1.css';
+
+function Nav1(){
+    return(
+        <div className="nav1_main">
+            <span  className="item1">Gift card</span>
+            <span className="item1">Best Sellers</span>
+            <span className="item1">Mobiles</span>
+            <span className="item1">Today's Deals</span>
+            <span className="item1">Prime</span>
+            <span className="item1">New Releases</span>
+            <span className="item1">Computers</span>
+            <span className="item1">Customer Service</span>
+            <span className="item1  hide1">Amazon Pay</span>
+            <span className="item1 hide1">Gift Ideas</span>
+            <span className="item1 hide1">Books</span>
+            <span className="item1 hide1">Sell</span>
+            <span className="item1 hide1">Baby</span>
+            <span className="item1 hide1">Amazon Basic</span>
+        </div>
+    );
+}
+export default Nav1;
