b28250ae93af9a993f5e5e4fea5b930ffa964357
Ritesh Singh <rootritesh64@gmail.com>
2020-12-10 13:22:17 +0530

    card file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Header from './Header/Header';
 import Nav1 from './Nav1/Nav1';
+import Cour from './coursel/coursel';
 function App() {
   return (
     <div className="App">
       <Header></Header>
       <Nav1></Nav1>
+      <Cour></Cour>
     </div>
   );
 }
---
+++ src/coursel/coursel.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './cour.css';
+// import Carousel from "react-elastic-carousel";
+function Cour(){
+    return(
+        
+
+        <div className="main_cor1">
+            {/* <Carousel  className="c1" enableAutoPlay autoPlaySpeed={3000}> */}
+            <div className="img1">   
+            <img  className="img11" src="https://images-eu.ssl-images-amazon.com/images/G/31/img19/AmazonPay/Boson/Sid/CCBP/Nov/BAU/Credit-Card-Bill_1500x600_without._CB415288872_.jpg"></img>
+            </div>
+            {/* <div>
+
+            <img src="https://images-eu.ssl-images-amazon.com/images/G/31/img2020/fashion/ApparelGW/ATF/U599/Sep/2._CB406752819_.jpg"></img>
+            </div>
+            <div>
+
+            <img src="https://images-eu.ssl-images-amazon.com/images/G/31/prime/Gateway/2020/May/gaming_1500x600._CB431281464_.jpg"></img>
+            </div> */}
+            {/* </Carousel> */}
+           
+        </div>
+      
+    );
+}
+
+export default Cour;
