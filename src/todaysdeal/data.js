0d2e26aee623fa55d6574d0cfff31f55bf558550
Ritesh Singh <rootritesh64@gmail.com>
2020-12-11 11:52:21 +0530

    making todays deal

--- src/todaysdeal/data.js
+++ src/todaysdeal/data.js
@@ -6,7 +6,7 @@ function Data(props){
     return(
         <div className="main_deal">
             <div className="main_deal2">
-            <h2 className="h2_deal">{props.title}</h2>
+            
                 <div className="content_deal">
                 <img  className =" image1"src={props.img}></img>
                 <p className="range">{props.range}</p>
--- src/todaysdeal/deal.js
+++ src/todaysdeal/deal.js
@@ -1,18 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Data from './data';
+import Carousel from "react-elastic-carousel";
+const breakPoints = [
+    { width: 1, itemsToShow: 1 , pagination: false},
+    { width: 0, itemsToShow: 2, itemsToScroll: 2 , pagination: false},
+    { width: 0, itemsToShow: 3 , pagination: false},
+    { width: 0, itemsToShow: 5 , pagination: false}
+  ];
 function Deal(){
     return(
+        <>
+        <h2 className="h2_deal">Today's Deal</h2>
+        {/* <h2>Today's Deal</h2> */}
         <div className="data_deal">
-        <Data
-        title="Today's Deal"
-        img="https://m.media-amazon.com/images/I/41TUs2Mms+L.__AC_SY200_.jpg"
-        range="₹51,999.00 - ₹58,999.00"
-        ></Data>
-        <Data
-        img="https://m.media-amazon.com/images/I/41TUs2Mms+L.__AC_SY200_.jpg"
-        range="₹51,999.00 - ₹58,999.00"
-        ></Data>
+            {/* <div className="h2_deal_main">
+            <h2 className="h2_deal">Today's Deal</h2>
+            </div> */}
+            {/* <Carousel itemsToShow="6" pagination="false" itemsToScroll="2"> */}
+        
+            <Carousel breakPoints={breakPoints} pagination="false">
         <Data
         img="https://m.media-amazon.com/images/I/41TUs2Mms+L.__AC_SY200_.jpg"
         range="₹51,999.00 - ₹58,999.00"
@@ -85,10 +92,19 @@ function Deal(){
         img="https://m.media-amazon.com/images/I/41TUs2Mms+L.__AC_SY200_.jpg"
         range="₹51,999.00 - ₹58,999.00"
         ></Data>
+        <Data
+        img="https://m.media-amazon.com/images/I/41TUs2Mms+L.__AC_SY200_.jpg"
+        range="₹51,999.00 - ₹58,999.00"
+        ></Data>
+        <Data
+        img="https://m.media-amazon.com/images/I/41TUs2Mms+L.__AC_SY200_.jpg"
+        range="₹51,999.00 - ₹58,999.00"
+        ></Data>
+        </Carousel>
         
         </div>
 
-
+            </>
     );
 }
 export default Deal;
